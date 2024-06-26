842014cc6b2d72edb3ab754f362affd80adae945
Kotalkhan366 <teamza366@gmail.com>
2024-06-25 17:47:37 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that adds two intergers and returns the result.
+ *
+ * Return: Always 0.
+ * */
+int addIntergers(int a, int b)
+{
+return a + b;
+}
+int main()
+{
+int num1 = 10;
+int num2 = 20;
+int sum = add interers(num1, num2);
+printf("The sum of %d and %d is &d\n", num1, num2, sum);
+return 0;
+}
