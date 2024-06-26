d497791fc59e69fc85395d15dc2fd976864b9ca2
Kotalkhan366 <teamza366@gmail.com>
2024-06-25 17:02:48 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that computes the value of an interger.
+ *
+ * Return: Always 0.
+ * */
+int main()
+{
+int num = -42;
+int absValue = abs(num);
+printf("Absolute value of %d is %d\n", num, absValue);
+return 0;
+}
